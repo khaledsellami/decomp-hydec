85d58784ae637e1c02333b02a327391ec9bfe493
khalsel <khaled.sellami.1@ulaval.ca>
2023-02-09 16:42:26 -0500

    updated single experiment file

--- single_experiment.py
+++ single_experiment.py
@@ -7,91 +7,67 @@ from hybridDecomp import HybridDecomp
 from analysis.dependencyAnalysis import DependencyAnalysis
 from analysis.tfidfAnalysis import TfidfAnalysis
 from analysis.sumAnalysis import SumAnalysis
-from user_config import DATA_PATH
-
-
-class DynamicAnalysis:
-    def __init__(self, traces_path, filtered_classes=[]):
-        self.traces_path = traces_path
-        with open(traces_path, 'r') as file:
-            traces = json.load(file)
-        self.filtered_classes = filtered_classes
-        self.parse_traces(traces)
-
-    def parse_traces(self, traces):
-        self.bcs = set()
-        self.bcs_per_class = dict()
-        self.bcs_per_DR = dict()
-        self.bcs_per_IR = dict()
-        for bc, bc_traces in traces.items():
-            self.bcs.add(bc)
-            for trace in set(bc_traces):
-                trace = trace.split(",")
-                for i in range(len(trace)):
-                    c1 = trace[i]
-                    if c1 in self.filtered_classes:
-                        continue
-                    if not c1 in self.bcs_per_class:
-                        self.bcs_per_class[c1] = set()
-                    self.bcs_per_class[c1].add(bc)
-                    for j in range(i+1,len(trace)):
-                        c2 = trace[j]
-                        if c2 in self.filtered_classes:
-                            continue
-                        if j==i+1:
-                            DR = (c1,c2)
-                            if not DR in self.bcs_per_DR:
-                                self.bcs_per_DR[DR] = set()
-                            self.bcs_per_DR[DR].add(bc)
-                        else:
-                            IR = (c1,c2)
-                            if not IR in self.bcs_per_IR:
-                                self.bcs_per_IR[IR] = set()
-                            self.bcs_per_IR[IR].add(bc)
-        self.class_names = list(self.bcs_per_class.keys())
-        self.bcs = list(self.bcs)
-        self.class_relations = np.zeros((len(self.class_names),len(self.class_names)))
-        self.class_bc_matrix = np.zeros((len(self.class_names),len(self.bcs)))
-        for i, c1 in enumerate(self.class_names):
-            for bc in self.bcs_per_class[c1]:
-                self.class_bc_matrix[i,self.bcs.index(bc)] = 1
-            for j, c2 in enumerate(self.class_names):
-                R = (c1,c2)
-                current_bcs  = set()
-                if R in self.bcs_per_DR:
-                    current_bcs = current_bcs.union(self.bcs_per_DR[R])
-                if R in self.bcs_per_IR:
-                    current_bcs = current_bcs.union(self.bcs_per_IR[R])
-                self.class_relations[i,j] += len(current_bcs)
+from analysis.bertAnalysis import BertAnalysis
+from analysis.wordEmbeddingAnalysis import WordEmbeddingAnalysis
+from analysis.embeddingModel import EmbeddingModel
+from user_config import DATA_PATH, DYN_DATA_PATH
+from utils.logTraceParser import LogTraceParcer
 
 
 if __name__ == "__main__":
     APP_DATA_PATH = DATA_PATH
     APP = "JPetStore"
     OUTPUT_PATH = os.path.join(os.path.curdir, "logs")
+
     # Initialize structural analysis
-    with open(os.path.join(APP_DATA_PATH, APP.lower(), "structural_data", "class_names.json"), "r") as f:
-        str_classes = json.load(f)
-    features = np.load(os.path.join(APP_DATA_PATH, APP.lower(), "structural_data", "class_calls.npy"))
-    structural_analysis = DependencyAnalysis(features, str_classes, str_classes)
-    # Initialize semantic analysis
+    # with open(os.path.join(APP_DATA_PATH, APP.lower(), "structural_data", "class_names.json"), "r") as f:
+    #     str_classes = json.load(f)
+    # features = np.load(os.path.join(APP_DATA_PATH, APP.lower(), "structural_data", "class_calls.npy"))
+    # structural_analysis = DependencyAnalysis(features, str_classes, str_classes)
+
+    # # Initialize semantic analysis
+    # with open(os.path.join(APP_DATA_PATH, APP.lower(), "semantic_data", "class_names.json"), "r") as f:
+    #     sem_classes = json.load(f)
+    # features = np.load(os.path.join(APP_DATA_PATH, APP.lower(), "semantic_data", "class_tfidf.npy"))
+    # with open(os.path.join(APP_DATA_PATH, APP.lower(), "semantic_data", "class_words.json"), "r") as f:
+    #     class_tokens = json.load(f)
+    # semantic_analysis = TfidfAnalysis(features, str_classes, str_classes, class_tokens)
+
+    # # combine structural and semantic analysis
+    # static_analysis = SumAnalysis([structural_analysis, semantic_analysis])
+
+    # Initialize semantic analysis with bert
     with open(os.path.join(APP_DATA_PATH, APP.lower(), "semantic_data", "class_names.json"), "r") as f:
         sem_classes = json.load(f)
     features = np.load(os.path.join(APP_DATA_PATH, APP.lower(), "semantic_data", "class_tfidf.npy"))
-    with open(os.path.join(APP_DATA_PATH, APP.lower(), "semantic_data", "class_words.json"), "r") as f:
-        class_tokens = json.load(f)
-    semantic_analysis = TfidfAnalysis(features, str_classes, str_classes, class_tokens)
-    # combine structural and semantic analysis
-    static_analysis = SumAnalysis([structural_analysis, semantic_analysis])
+    with open(os.path.join(APP_DATA_PATH, APP.lower(), "static_analysis_results", "typeData.json"), "r") as f:
+        class_text = [c["textAndNames"] for c in json.load(f)]
+    embeddings = np.load(os.path.join(APP_DATA_PATH, APP.lower(), "embeddings.npy"))
+    bert_analysis = BertAnalysis(class_text, sem_classes, sem_classes, aggregation="mean", embeddings=embeddings)
+    # np.save(os.path.join(APP_DATA_PATH, APP.lower(), "embeddings.npy"),bert_analysis.embeddings)
+
+    # Initialize semantic analysis with fastext
+    with open(os.path.join(APP_DATA_PATH, APP.lower(), "semantic_data", "class_names.json"), "r") as f:
+        sem_classes = json.load(f)
+    features = np.load(os.path.join(APP_DATA_PATH, APP.lower(), "semantic_data", "class_tfidf.npy"))
+    with open(os.path.join(APP_DATA_PATH, APP.lower(), "static_analysis_results", "typeData.json"), "r") as f:
+        class_text = [c["textAndNames"] for c in json.load(f)]
+    embedding_model = EmbeddingModel()
+    embedding_model.load_embedding_model()
+    class_tokens = [embedding_model.tokenize(text) for text in class_text]
+    emb_analysis = WordEmbeddingAnalysis(class_text, embedding_model, sem_classes, sem_classes)
+
     # Initialize dynamic analysis
-    dynamic_analysis_data = "/Users/khalsel/Documents/projects/hybridDecomp/applications/jpetstore/context_traces.json"
-    dyn_analysis = DynamicAnalysis(dynamic_analysis_data)
+    dynamic_analysis_data = DYN_DATA_PATH
+    dyn_analysis = LogTraceParcer(dynamic_analysis_data)
     short_names = [c.split(".")[-1] for c in sem_classes]
     filtered_classes = [i for i in dyn_analysis.class_names if i.split("::")[-1] not in short_names]
-    dyn_analysis = DynamicAnalysis(dynamic_analysis_data, filtered_classes)
-    dynamic_analysis = DependencyAnalysis(dyn_analysis.class_relations, short_names, [i.split("::")[-1] for i in dyn_analysis.class_names])
+    dyn_analysis = LogTraceParcer(dynamic_analysis_data, filtered_classes)
+    dynamic_analysis = DependencyAnalysis(dyn_analysis.class_relations, short_names,
+                                          [i.split("::")[-1] for i in dyn_analysis.class_names])
+
     # Create the clustering object
-    hybrid_decomp = HybridDecomp([dynamic_analysis, static_analysis], sem_classes, [0.65, 0.65])
+    hybrid_decomp = HybridDecomp([dynamic_analysis, bert_analysis], sem_classes, [0.65, 0.05])
     layers = hybrid_decomp.cluster()
     for i, layer in enumerate(layers):
         print(i, layer)
