ceecdbaa1f7c6c7a38a5b2592d98e229003687a1
khalsel <khaled.sellami.1@ulaval.ca>
2023-09-06 13:51:19 -0400

    bert analysis hotfix

--- analysis/bertAnalysis.py
+++ analysis/bertAnalysis.py
@@ -11,10 +11,10 @@ from .similarity import cosine_similarity
 
 
 class BertAnalysis(AbstractAnalysis):
-    SUPPORTED_AGGREGATION = ["mean", "sum"]
+    SUPPORTED_AGGREGATION = ["mean", "sum", "combine"]
 
     def __init__(self, atom_tokens: List[List[str]], atoms: List, supported_atoms: List,
-                 model_name: str = "bert-base-uncased", aggregation="mean", embeddings = None):
+                 model_name: str = "bert-base-uncased", aggregation: str = "mean", embeddings=None):
         if aggregation not in self.SUPPORTED_AGGREGATION:
             raise ValueError("Unsupported aggregation method \"{}\"".format(aggregation))
         # change type of word to those without preprocessing
@@ -24,9 +24,10 @@ class BertAnalysis(AbstractAnalysis):
         self.aggregation = aggregation
         self.features = atom_tokens
         self.model_name = model_name
+        self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.data_collator = DataCollatorWithPadding(tokenizer=self.tokenizer)
-        self.model = BertModel.from_pretrained(model_name)
+        self.model = BertModel.from_pretrained(model_name).to(self.device)
         for param in self.model.named_parameters():
             param[1].requires_grad=False
         self.embeddings = None
@@ -36,16 +37,17 @@ class BertAnalysis(AbstractAnalysis):
             self.embeddings = self.get_embeddings(self.features)
 
     def get_embeddings(self, features):
-        dataset = self.tokenizer([" ".join(x) for x in features], truncation=True, padding=True, return_tensors="pt")
+        dataset = self.tokenizer(
+            [" ".join(x) for x in features], truncation=True, padding=True, return_tensors="pt")
         # dataset = Dataset.from_dict({"words": features}).map(lambda x: {"sentence": " ".join(x["words"])})
         # dataset = dataset.map(lambda x: self.tokenizer(
         #     x["sentence"], truncation=True, padding=True, return_tensors="pt"), batched=True)
         #dataset = Dataset.from_dict({"words": features}).map(f, batched=True)
-        print(torch.LongTensor(dataset["input_ids"]).shape)
-        output = self.model(input_ids=torch.LongTensor(dataset["input_ids"]),
-                            attention_mask=torch.LongTensor(dataset["attention_mask"]),
+        # print(torch.LongTensor(dataset["input_ids"]).shape)
+        output = self.model(input_ids=torch.LongTensor(dataset["input_ids"]).to(self.device),
+                            attention_mask=torch.LongTensor(dataset["attention_mask"]).to(self.device),
                             return_dict=False)
-        embeddings = output[0][:, 0, :].view(-1, 768).detach().numpy()
+        embeddings = output[0][:, 0, :].view(-1, 768).cpu().detach().numpy()
         return embeddings
 
     def aggregate(self, current_clusters: List[int]):
@@ -59,7 +61,6 @@ class BertAnalysis(AbstractAnalysis):
         elif self.aggregation == "combine":
             new_words = list()
             for j in range(aggregation_map.shape[1]):
-                print(np.where(aggregation_map[:, j])[0])
                 new_words.append(list(itertools.chain(*[self.features[i] for i in np.where(aggregation_map[:, j])[0]])))
             features = self.get_embeddings(new_words)
         else:
