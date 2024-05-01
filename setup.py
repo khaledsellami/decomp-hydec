fd7e5ccb7f63f7c1f8708a26b5e06cf538c955e6
khalsel <khaled.sellami.1@ulaval.ca>
2024-04-30 21:08:50 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 # setup.py
 from setuptools import setup, find_packages
-
-from hydec import __version__
+import os
+import re
 
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
+
+with open(os.path.join(os.path.dirname(__file__), "hydec", "_version.py")) as f:
+    version_file = f.read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        __version__ = version_match.group(1)
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 if __name__ == "__main__":
     setup(
